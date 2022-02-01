20a9f42597b60c59faede7eb31b8d45ed5384910
Akshat Rana <akshrana20@gmail.com>
2022-02-01 23:31:57 +0530

    added edit route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 8080;
+const {
+    v1: uuidv1,
+    v4: uuidv4,
+} = require('uuid');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/view'));
@@ -10,11 +14,14 @@ app.use(express.static('assets'));
 app.use(express.urlencoded({
     extended: true
 }));
+
+
 const detail = {};
 detail.education_detail = [];
 detail.project = [];
 detail.work_experience = [];
 detail.achievement = [];
+
 
 app.get("/", (req, res) => {
     res.render('index', {
@@ -38,30 +45,58 @@ app.get('/work-experience', (req, res) => {
 });
 app.get('/achievement', (req, res) => {
     res.render('achievement');
-})
+});
+
+// edited
+app.get('/personal-detail/:id', (req, res) => {
+    res.send(`hello personal detail with id:${req.params.id}`);
+});
+app.get('/education-detail/:id', (req, res) => {
+    res.send(`hello education detail with id:${req.params.id}`);
+});
+app.get('/project/:id', (req, res) => {
+    res.send(`hello project detail with id:${req.params.id}`);
+});
+app.get('/achievements/:id', (req, res) => {
+    res.send(`hello personal detail with id:${req.params.id}`);
+});
+app.get('/work-experience/:id', (req, res) => {
+    res.send(`hello personal detail with id:${req.params.id}`);
+});
+app.get('/technical-skills/:id', (req, res) => {
+    res.send(`hello personal detail with id:${req.params.id}`);
+});
+
+
 
 
 app.post("/personal-detail", (req, res) => {
+    req.body.id = uuidv4();
     detail.personal_detail = req.body;
     res.redirect('/');
 });
 app.post("/education-detail", (req, res) => {
+    req.body.id = uuidv4();
     detail.education_detail.push(req.body);
     res.redirect('/');
 });
 app.post("/technical-skills", (req, res) => {
+    req.body.id = uuidv4();
     detail.technical_skills = req.body;
     res.redirect('/');
 });
 app.post('/project', (req, res) => {
+    req.body.id = uuidv4();
     detail.project.push(req.body);
     res.redirect('/');
 });
 app.post('/work-experience', (req, res) => {
+    req.body.id = uuidv4();
     detail.work_experience.push(req.body);
     res.redirect('/');
 });
 app.post('/achievement', (req, res) => {
+    req.body.id = uuidv4();
     detail.achievement.push(req.body);
     res.redirect('/');
 });
